c53377561a10074067aac85999447225ec315c71
James Cao <wjfsryx@qq.com>
2018-06-24 19:55:13 +0800

    first commit

---
+++ src/main/java/com/wjfsryx/readlinglist/ReadlinglistApplication.java
@@ -0,0 +1,12 @@
+package com.wjfsryx.readlinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadlinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadlinglistApplication.class, args);
+	}
+}
